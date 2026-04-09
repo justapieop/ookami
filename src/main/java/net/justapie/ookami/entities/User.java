5eed250152ed16380f2f769dbc25b5974b359a4c
Do Tran Thai An <justapie06@gmail.com>
2026-04-10 01:52:40 +0700

    feat: create User datasource entity and UserRepository datasource handler

---
+++ src/main/java/net/justapie/ookami/entities/User.java
@@ -0,0 +1,23 @@
+package net.justapie.ookami.entities;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+
+import java.io.Serializable;
+import java.util.UUID;
+
+@Getter
+@Entity
+@Table(
+        name = "users",
+        indexes = {
+                @Index(columnList = "id", unique = true),
+                @Index(columnList = "username", unique = true)
+        })
+public class User implements Serializable {
+    @Id
+    private UUID id;
+
+    @Column(unique = true)
+    private String username;
+}
---
+++ src/main/java/net/justapie/ookami/repositories/UserRepository.java
@@ -0,0 +1,16 @@
+package net.justapie.ookami.repositories;
+
+import net.justapie.ookami.entities.User;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.Repository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.UUID;
+
+public interface UserRepository extends Repository<User, UUID> {
+    @Query("SELECT u FROM User u WHERE u.id = :id")
+    User findUserById(@Param("id") UUID id);
+
+    @Query("SELECT u FROM User u WHERE u.username = :username")
+    User findUserByUsername(@Param("username") String username);
+}
