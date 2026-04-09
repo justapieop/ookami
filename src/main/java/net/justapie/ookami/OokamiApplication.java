acacae560d3d687303eca5dade24074f5dba232d
Do Tran Thai An <justapie06@gmail.com>
2026-04-10 01:50:44 +0700

    feat: init Spring Boot application

---
+++ src/main/java/net/justapie/ookami/OokamiApplication.java
@@ -0,0 +1,11 @@
+package net.justapie.ookami;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OokamiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(OokamiApplication.class, args);
+    }
+}
---
+++ src/test/java/net/justapie/ookami/OokamiApplicationTests.java
@@ -0,0 +1,12 @@
+package net.justapie.ookami;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OokamiApplicationTests {
+    @Test
+    void contextLoads() {
+
+    }
+}
