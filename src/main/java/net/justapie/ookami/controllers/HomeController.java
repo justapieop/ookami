10bdbecd2ed0411f06ab2deddb570db487366da3
Do Tran Thai An <justapie06@gmail.com>
2026-04-11 16:00:03 +0700

    feat: add tag for home controller

--- src/main/java/net/justapie/ookami/controllers/HomeController.java
+++ src/main/java/net/justapie/ookami/controllers/HomeController.java
@@ -1,10 +1,12 @@
 package net.justapie.ookami.controllers;
 
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.Serializable;
 
+@Tag(name = "Home")
 @RestController("/")
 public class HomeController {
     public record HomeResponse(String status) implements Serializable {
