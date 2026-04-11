948c1f6adc61fe9147ac8e15154ec1d416ddc07b
Do Tran Thai An <justapie06@gmail.com>
2026-04-11 10:34:14 +0700

    feat: add SignatureUtils to check for webhook signature

---
+++ src/main/java/net/justapie/ookami/utils/SignatureUtils.java
@@ -0,0 +1,42 @@
+package net.justapie.ookami.utils;
+
+import lombok.experimental.UtilityClass;
+
+import javax.crypto.Mac;
+import javax.crypto.spec.SecretKeySpec;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+
+@UtilityClass
+public class SignatureUtils {
+    public String hmacSHA256String(byte[] data, byte[] secret) throws NoSuchAlgorithmException, InvalidKeyException {
+        Mac hasher = Mac.getInstance("HmacSHA256");
+        SecretKeySpec keySpec = new SecretKeySpec(secret, "HmacSHA256");
+        hasher.init(keySpec);
+        byte[] signature = hasher.doFinal(data);
+        return bytesToHex(signature);
+    }
+
+    private String bytesToHex(byte[] bytes) {
+        StringBuilder result = new StringBuilder();
+        for (byte b : bytes) {
+            result.append(String.format("%02x", b));
+        }
+        return result.toString();
+    }
+
+    private static boolean constantTimeCompare(String a, String b) {
+        if (a.length() != b.length()) {
+            return false;
+        }
+
+        byte[] aBytes = a.getBytes();
+        byte[] bBytes = b.getBytes();
+
+        int result = 0;
+        for (int i = 0; i < aBytes.length; i++) {
+            result |= aBytes[i] ^ bBytes[i];
+        }
+        return result == 0;
+    }
+}
