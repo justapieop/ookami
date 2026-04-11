08899e30d24f57dbca99407da8a5b4b5b4031108
Do Tran Thai An <justapie06@gmail.com>
2026-04-11 16:00:23 +0700

    feat: add dto to update username

---
+++ src/main/java/net/justapie/ookami/controllers/users/UpdateUsernameDto.java
@@ -0,0 +1,14 @@
+package net.justapie.ookami.controllers.users;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class UpdateUsernameDto {
+    @NotBlank
+    @Size(min = 1, max = 255)
+    private String username;
+}
